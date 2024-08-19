5d4ec0405cc572bfb5df0f3907ad40c14f448760
Mastermind730 <72343319+Mastermind730@users.noreply.github.com>
2024-08-19 16:49:48 +0530

    done with the register users api

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -5,17 +5,25 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.exceptions import ValidationError
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
+        print("Request data:", request.data)
+        
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            print("Validation Error:", serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
